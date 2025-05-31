b697b0b9e3b0e6f8d8d7c1b09aff24ee3ff63411
jeff <jeff@4sitemedia.com>
2025-05-31 18:22:31 -0400

    add data attribution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Laravel / Vue NHL Schedule Viewer:\
 https://hockey.4sitemedia.com/
 
-&copy; 4SiteMedia LLC. All Rights Reserved.
+&copy; 4SiteMedia LLC. All Rights Reserved. Data sourced from [nhl.com](https://www.nhl.com/).
--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -3,5 +3,8 @@ const year = new Date().getFullYear();
 </script>
 
 <template>
-    <div class="mt-auto py-4">&copy; {{ year }} 4SiteMedia LLC. All Rights Reserved.</div>
+    <div class="mt-auto py-4">
+        &copy; {{ year }} 4SiteMedia LLC. All Rights Reserved. Data sourced from
+        <a href="https://www.nhl.com/">nhl.com</a>.
+    </div>
 </template>
